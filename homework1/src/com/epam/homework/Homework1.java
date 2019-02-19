d1df28d6243aa5d45f7843762413f4abe61b3178
Boiko Anastasiia <boye4ka@gmail.com>
2019-02-20 00:33:38 +0200

    created repository

---
+++ homework1/src/com/epam/homework/Homework1.java
@@ -0,0 +1,15 @@
+package com.epam.homework;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        System.out.println("* * * *  * * * *        *       *         *");
+        System.out.println("                                           ");
+        System.out.println("*        *     *      *   *     *  *   *  *");
+        System.out.println("                                           ");
+        System.out.println("* *      * * * *     *     *    *    *    *");
+        System.out.println("                                           ");
+        System.out.println("*        *          * * * * *   *         *");
+        System.out.println("                                           ");
+        System.out.println("* * * *  *         *         *  *         *");
+    }
+}
